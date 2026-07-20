9d16206cdbb223500eb0cb0a3267214e19aec373
SamTeoli <qswa1q@gmail.com>
2026-07-20 20:46:22 +0900

    39A~39C 적 자동 공격: BattleRuntimeEnemyRepeatedAttackFailure.cs 추가

---
+++ Assets/Scripts/Cards/BattleRuntimeEnemyRepeatedAttackFailure.cs
@@ -0,0 +1,11 @@
+namespace HaveABreak.Cards
+{
+    public enum BattleRuntimeEnemyRepeatedAttackFailure
+    {
+        None,
+        InvalidRuntime,
+        InvalidAttackCount,
+        MissingTieBreakerValues,
+        AttackFailed
+    }
+}
