059fb9d7c5dd158afdc66902919dff850594c6df
SamTeoli <qswa1q@gmail.com>
2026-07-20 16:06:08 +0900

    적 능력 해결 실패 사유 추가

---
+++ Assets/Scripts/Cards/BattleRuntimeEnemyAbilityFailure.cs
@@ -0,0 +1,11 @@
+namespace HaveABreak.Cards
+{
+    public enum BattleRuntimeEnemyAbilityFailure
+    {
+        None,
+        InvalidRuntime,
+        InvalidTurnPhase,
+        InvalidAbility,
+        InvalidSourceEnemy
+    }
+}
