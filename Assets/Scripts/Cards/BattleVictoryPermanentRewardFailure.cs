a13bab1bb518776c9a9baf1156e088bb18731543
SamTeoli <qswa1q@gmail.com>
2026-07-20 23:58:40 +0900

    Connect final boss permanent rewards (8/13)

---
+++ Assets/Scripts/Cards/BattleVictoryPermanentRewardFailure.cs
@@ -0,0 +1,16 @@
+namespace HaveABreak.Cards
+{
+    public enum BattleVictoryPermanentRewardFailure
+    {
+        None,
+        InvalidEncounterContext,
+        InvalidRewardState,
+        SettlementNotComplete,
+        NotVictory,
+        NotFinalBoss,
+        AlreadyCreated,
+        AlreadyClaimed,
+        InvalidRewardId,
+        RewardAlreadyOwned
+    }
+}
